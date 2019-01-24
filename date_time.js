2fdc5b09e1110abea7cfc46975694a92d702c5f9
Octavio Palomeque <octaviop_95@hotmail.com>
2019-01-24 11:33:18 -0600

    Ya funciona la hora del servidor y el cliente, falta que se actualice en tiempo real

--- date_time.js
+++ date_time.js
@@ -18,11 +18,13 @@ var refDateTime = new Date();
              } catch(err2) { /* swallow it */ }
           }
           if (xmlHttp) {
-             xmlHttp.open('HEAD', window.location.href.toString(), false);
+             xmlHttp.open('HEAD', 'http://127.0.0.1:8081/', false);
              xmlHttp.setRequestHeader("Content-Type", "text/html");
              xmlHttp.send('');
              var serverDateTime = xmlHttp.getResponseHeader("Date");
              if (serverDateTime) {
+				 var refresh=1000; // Refresh rate in milli seconds
+				 mytime = setTimeout('display_ct()',refresh)
                  var dateNow = new Date();
                  var serverDate = new Date(serverDateTime);
                  var delta = serverDate.getTime() - dateNow.getTime();
@@ -38,3 +40,37 @@ var refDateTime = new Date();
        }
        return serverTimeDelta;
     }
+
+/* var xmlHttp;
+function srvTime(){
+	try {
+		//FF, Opera, Safari, Chrome
+		xmlHttp = new XMLHttpRequest();
+	}
+	catch (err1) {
+		//IE
+		try {
+			xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
+			document.write("hola<br>");
+		}
+		catch (err2) {
+			try {
+				xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+				document.write("hola2<br>");
+			}
+			catch (eerr3) {
+				//AJAX not supported, use CPU time.
+				alert("AJAX not supported");
+			}
+		}
+	}
+	xmlHttp.open('GET','http://127.0.0.1:8081/',true);
+	xmlHttp.setRequestHeader("Content-Type", "text/html");
+	document.write("hola5<br>");
+	xmlHttp.send('');
+	document.write("hola5<br>");
+	return xmlHttp.getResponseHeader("Date");
+}
+
+var st = srvTime();
+var date = new Date(st); */
